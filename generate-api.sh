46be0e267f79e36af7f0e79a73b40e465e7117da
Ivo Bellin Salarin <ivo@lumapps.com>
2018-09-19 10:49:09 +0200

    feat: generated api files

---
+++ generate-api.sh
@@ -0,0 +1,10 @@
+# generate cloudsearch api client
+echo "$GOPATH/bin/google-api-go-generator" -cache=false -api_json_file="$GOPATH/src/github.com/nilleb/google-api-go-client/cloudsearch-v1beta1.json" -install -api_pkg_base github.com/nilleb/google-api-go-client -output "$GOPATH/src/github.com/nilleb/google-api-go-client/cloudsearch/v1beta1/cloudsearch-gen.go"
+"$GOPATH/bin/google-api-go-generator" -cache=false -api_json_file="$GOPATH/src/github.com/nilleb/google-api-go-client/cloudsearch-v1beta1.json" -install -api_pkg_base github.com/nilleb/google-api-go-client -output "$GOPATH/src/github.com/nilleb/google-api-go-client/cloudsearch/v1beta1/cloudsearch-gen.go"
+
+echo "$GOPATH/bin/google-api-go-generator" -cache=false -api_json_file="$GOPATH/src/github.com/nilleb/google-api-go-client/cloudsearch-v1.json" -install -api_pkg_base github.com/nilleb/google-api-go-client -output "$GOPATH/src/github.com/nilleb/google-api-go-client/cloudsearch/v1/cloudsearch-gen.go"
+"$GOPATH/bin/google-api-go-generator" -cache=false -api_json_file="$GOPATH/src/github.com/nilleb/google-api-go-client/cloudsearch-v1.json" -install -api_pkg_base github.com/nilleb/google-api-go-client -output "$GOPATH/src/github.com/nilleb/google-api-go-client/cloudsearch/v1/cloudsearch-gen.go"
+
+# generate enterprise grade groups api client
+echo "$GOPATH/bin/google-api-go-generator" -cache=false -api_json_file="$GOPATH/src/github.com/nilleb/google-api-go-client/enterprisegradegroups.json" -install -api_pkg_base github.com/nilleb/google-api-go-client -output "$GOPATH/src/github.com/nilleb/google-api-go-client/groups/v1alpha/cloudidentity-gen.go"
+"$GOPATH/bin/google-api-go-generator" -cache=false -api_json_file="$GOPATH/src/github.com/nilleb/google-api-go-client/enterprisegradegroups.json" -install -api_pkg_base github.com/nilleb/google-api-go-client -output "$GOPATH/src/github.com/nilleb/google-api-go-client/groups/v1alpha/cloudidentity-gen.go"
